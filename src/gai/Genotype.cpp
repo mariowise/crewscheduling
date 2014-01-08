64ea5e25efd0d481aaae0be92b693d6f43bd50b2
mario <mario@mario-elementary.(none)>
2014-01-08 10:36:32 -0300

    GAGenome binario. Solo falta implementar evaluador

--- src/gai/Genotype.cpp
+++ src/gai/Genotype.cpp
@@ -1,25 +1,37 @@
-#include <ga/GAGenome.h>
-#include <ga/GASimpleGA.h>
-#include <ga/std_stream.h>
-#include <ga/ga.h>
+#include <iostream>
+#include <ctime>
 
 #include <gai/Genotype.h>
 #include <Common.h>
 
 using namespace std;
 
-void Genotype::stripInitializer(GAGenome & genoma) {
-	
-}
+float Genotype::evaluator(GAGenome & dude) {
 
-float Genotype::stripEvaluator(GAGenome & genoma) {
 	return 0.0f;
 }
 
-int Genotype::stripCrossover(const GAGenome& father, const GAGenome& mother, GAGenome* brother, GAGenome* sister) {
-	return 0;
-}
+void Genotype::main() {
+	GARandomSeed(time(NULL));
 
-int Genotype::stripMutator(GAGenome & genoma, float pmut) {
-	return 0;
+	// Configuración de GA
+	int genotypeLength = trips.size() * trips.size();
+	int popSize = 50;
+	int nGen = 400;
+	float pMut = 0.001;
+	float pCross = 0.9;
+
+	// Se crea el objeto genoma
+	GA1DBinaryStringGenome genoma(genotypeLength, Genotype::evaluator);
+
+	// Lanzamiento del algoritmo genético
+	GASimpleGA ga(genoma);
+	ga.populationSize(popSize);
+	ga.nGenerations(nGen);
+	ga.pMutation(pMut);
+	ga.pCrossover(pCross);
+	ga.evolve(); // Launch
+
+	// El wladi de la wea
+	cout << "The GA found:\n" << ga.statistics().bestIndividual() << "\n";
 }
